5699da512a6755fee5f94bec0c58016af0ca5b97
Umar Farooq <umarfarooq211203@gmail.com>
2024-07-26 21:55:02 +0500

    Testing Code Completed That saves the filters and results as images

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ def get_largest_contour(mask):
 
     
 
-def detect_road_bleed(image,mask,pothole,threshold = 15):
+def detectRoadBleed(image,mask,pothole,threshold = 15,name = ''):
+    
+    if image is None or mask is None or pothole is None:
+        return None
+    
     bin_mask = binary_mask(mask)
     
     
@@ -21,17 +25,14 @@ def detect_road_bleed(image,mask,pothole,threshold = 15):
     else:
         gray_image = image.copy()
     
-    _, binary_image = cv2.threshold(gray_image, 50, 255, cv2.THRESH_BINARY)
+    _, binary_image = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)
     binary_image = cv2.bitwise_and(bin_mask,binary_image)
     shadow_filter = cv2.bitwise_not(get_largest_contour(binary_image))
 
     pothole_filter = binary_mask(pothole)
     cracks_filter = filter_color(mask,(0,0,255))
     
-    #Temporary code
     pothole_filter = cv2.cvtColor(pothole.copy(),cv2.COLOR_BGR2GRAY)
-    print(np.mean(pothole_filter))
-    print(np.std(pothole_filter))
     _,pothole_filter = cv2.threshold(pothole_filter,np.mean(pothole_filter)+2*np.std(pothole_filter),255,cv2.THRESH_BINARY)
     pothole_filter = binary_mask(pothole_filter)
 
@@ -45,12 +46,10 @@ def detect_road_bleed(image,mask,pothole,threshold = 15):
     resultant_image = cv2.morphologyEx(binary_thresholded_image,cv2.MORPH_OPEN,kernel,iterations=5)
     resultant_image = binary_mask(resultant_image)
     opened = resultant_image.copy()
-    # cv2.imshow("res",resize_image(binary_thresholded_image))
 
     
     
     resultant_image = cv2.subtract(resultant_image,final_filter)
-    # cv2.imshow('resultant_image',resize_image(resultant_image))
     w = image.shape[0]
     
     joined1 = join_images_horizontally([cracks_filter,pothole_filter,shadow_filter])
@@ -63,31 +62,39 @@ def detect_road_bleed(image,mask,pothole,threshold = 15):
     cv2.putText(joined2,"Final Filter",(w-int(0.4*w),300),1,15,150,5)
     cv2.putText(joined2,"Result",(int(1.5*w),300),1,15,150,5)
     
-    cv2.imshow('Cracks+Potholes+Shadows',resize_image(joined1))
-    cv2.imshow('Image+Filter+Result',resize_image(joined2))
+    # cv2.imwrite(f'{name}_filters',resize_image(joined1))
+    # cv2.imwrite(f'{name}_results',resize_image(joined2))
+    cv2.imwrite(f'{name}_filters.png',resize_image(joined1))
+    cv2.imwrite(f'{name}_results.png',resize_image(joined2))
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
     return apply_threshold(resultant_image,threshold)
-        
+## Ignore the Code Below
+masks = []
+names = []
+potholes = []
+for i in range (0,10):
+    names.append(f'Dataset/images2/0000000{i}.png')
+    masks.append(f'Dataset/masks2/0000000{i}.png')
+    potholes.append(f'Dataset/dm2/0000000{i}.png')
+for i in range (0,6):
+    names.append(f'Dataset/images2/0000001{i}.png')
+    masks.append(f'Dataset/masks2/0000001{i}.png')
+    potholes.append(f'Dataset/dm2/0000001{i}.png')
 
-    
-# image = cv2.imread('image.png')
-# mask = cv2.imread('mask.png')
-# pothole = cv2.imread('depthmap.png')
+
+for i in range(len(names)):
+    image = cv2.imread(names[i])
+    mask = cv2.imread(masks[i])
+    pothole = cv2.imread(potholes[i])
+    print(f"{names[i]}:    {detectRoadBleed(image,mask,pothole,20,f"Results/{i}")}")
+
+
+# image = cv2.imread('Dataset/images2/00000001.png')
+# mask = cv2.imread('Dataset/masks2/00000001.png')
+# pothole = cv2.imread('Dataset/dm2/00000001.png')
 # detect_road_bleed(image,mask,pothole)
 
 
-
-
-
-
-
-
-
-
-image = cv2.imread('Dataset/images2/00000001.png')
-mask = cv2.imread('Dataset/masks2/00000001.png')
-pothole = cv2.imread('Dataset/dm2/00000001.png')
-detect_road_bleed(image,mask,pothole)
